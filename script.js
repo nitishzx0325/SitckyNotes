b7a59f1e1384062a5ce645a11ca2675bdc0030cc
nitishzx0325 <nitishzx0325@gmail.com>
2022-05-29 20:02:14 +0530

    done with oprations

--- script.js
+++ script.js
@@ -12,7 +12,43 @@ let modalPriorityColor = color[color.length - 1];
 let allPriorityColor = document.querySelectorAll(".priority-color");
 let lockClass = "fa-lock";
 let unlockClass = "fa-lock-open";
+let toolboxColors = document.querySelectorAll(".color");
+let ticketArray = [];
 //Listerner for modal coloring
+for (let i = 0; i < toolboxColors.length; i++) {
+  toolboxColors[i].addEventListener("click", (e) => {
+    let currentToolBoxColor = toolboxColors[i].classList[0];
+    let filteredTickets = ticketArray.filter((ticketobj, idx) => {
+      return currentToolBoxColor === ticketobj.ticketColor;
+    });
+    // remove prev ticket
+    let allTicketCount = document.querySelectorAll(".ticket-cont");
+    for (let i = 0; i < allTicketCount.length; i++) {
+      allTicketCount[i].remove();
+    }
+    // display new filtered tickets
+    filteredTickets.forEach((ticketobj, idx) => {
+      createTicket(
+        ticketobj.ticketColor,
+        ticketobj.ticketTask,
+        ticketobj.ticketID
+      );
+    });
+  });
+  toolboxColors[i].addEventListener("dblclick", (e) => {
+    let allTicketCount = document.querySelectorAll(".ticket-cont");
+    for (let i = 0; i < allTicketCount.length; i++) {
+      allTicketCount[i].remove();
+    }
+    ticketArray.forEach((ticketobj, idx) => {
+      createTicket(
+        ticketobj.ticketColor,
+        ticketobj.ticketTask,
+        ticketobj.ticketID
+      );
+    });
+  });
+}
 allPriorityColor.forEach((colorEle, idx) => {
   colorEle.addEventListener("click", (e) => {
     allPriorityColor.forEach((priorityColrEle, idx) => {
@@ -22,6 +58,13 @@ allPriorityColor.forEach((colorEle, idx) => {
     modalPriorityColor = colorEle.classList[0];
   });
 });
+
+for (let i = 0; i < toolboxColors.length; i++) {
+  toolboxColors[i].addEventListener("click", (e) => {
+    let currentTollBoaxColor = toolboxColors[i].classList[0];
+  });
+}
+
 addbtn.addEventListener("click", function (e) {
   console.log("clicked");
   // display ticket
@@ -44,15 +87,19 @@ removebtn.addEventListener("click", (e) => {
 function createTicket(ticketColor, ticketTask, ticketID) {
   let ticketCount = document.createElement("div");
   console.log("cccc!!!!");
+  let id = ticketID || shortid();
+  // let ticketCont = document.createElement("div");
   ticketCount.setAttribute("class", "ticket-cont");
   ticketCount.innerHTML = `
 
   <div class="ticket-color ${ticketColor}"></div>
-  <div class="ticket-id">#${ticketID}</div>
+  <div class="ticket-id">#${id}</div>
   <div class="task-area">${ticketTask}</div>
   <div class="ticket-lock">
   <i class="fa-solid fa-lock"></i> </div>`;
   mainCont.appendChild(ticketCount);
+  // create object of array and add to array
+  if (!ticketID) ticketArray.push({ ticketColor, ticketTask, ticketID: id });
   handleRemoval(ticketCount);
   handlelock(ticketCount);
   handleColor(ticketCount);
@@ -99,9 +146,24 @@ modal.addEventListener("keyup", function (e) {
   console.log(e.key);
   let p = e.key;
   if (p == "shift" || p == "Shift") {
-    createTicket(modalPriorityColor, textAreaCont.value, shortid());
-    modal.style.display = "none";
+    createTicket(modalPriorityColor, textAreaCont.value);
+    // modal.style.display = "none";
+    // textAreaCont.value = "";
     flag = false;
-    textAreaCont.value = "";
+    setModalDefualt();
   }
 });
+function setModalDefualt() {
+  modal.style.display = "none";
+  textAreaCont.value = "";
+  modalPriorityColor = color[color.length - 1];
+  allPriorityColor.forEach((priorityColrEle, idx) => {
+    priorityColrEle.classList.remove("border");
+  });
+  allPriorityColor[allPriorityColor.length - 1].classList.add("border");
+  // let allTicketCount = document.querySelectorAll(".ticket-cont");
+  // for (let i = 0; i < allTicketCount.length; i++) {
+  //   allTicketCount[i].remove();
+  // }
+  // allPriorityColor[allPriorityColor.length - 1].classList.add("border");
+}
